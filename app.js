23f925d3e702ec527abbb25eb1b7ba8908e812a2
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-10-03 14:11:54 -0400

    Created initial snake and deployed to surge

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const gameBoard = document.querySelector('.game-board');
+const numberOfSquares = 256;
+const snakeSquares = [];
+let numberOfSnakeSquares = 3;
+let itemSquare;
+let itemSquareNumber;
 
 const createGameBoard = () => {
-	for (let i = 0; i < 16; i++) {
-		str = 'abcdefghijklmnop';
-		letter = str[i].toUpperCase();
-		for (let j = 0; j < 16; j++) {
-			square = document.createElement('div');
-			square.classList.add('square');
-			square.setAttribute('id', `${letter}${j}`);
-			gameBoard.appendChild(square);
-		}
+	for (let i = 0; i < numberOfSquares; i++) {
+		let square = document.createElement('div');
+		square.classList.add('square');
+		square.setAttribute('id', `${i}`);
+		gameBoard.appendChild(square);
+	}
+	const squares = document.querySelectorAll('.square');
+	const allSquares = [...squares];
+	return allSquares;
+};
+
+const allSquares = createGameBoard();
+const emptySquares = allSquares;
+
+const createSnake = () => {
+	let snakeHead = Math.floor(numberOfSquares / 2) + 4;
+	for (let i = 0; i < 3; i++) {
+		let squareNumber = snakeHead - i;
+		snakeSquares.push(squareNumber);
+		let square = allSquares[squareNumber - 1];
+		square.classList.add('snake');
+		emptySquares.splice(emptySquares.indexOf(square), 1);
 	}
 };
 
-createGameBoard();
+createSnake();
