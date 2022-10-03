fa23452a1f62147ee3f9a9d3f93adc06fea79763
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-10-03 19:59:06 -0400

    Got the snake to move in all directions

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Global variables
+
 const gameBoard = document.querySelector('.game-board');
 const numberOfSquares = 256;
 const numberOfSquaresIndex = numberOfSquares - 1;
@@ -5,9 +7,14 @@ const rowLength = Math.sqrt(numberOfSquares);
 const allSquares = [];
 const emptySquares = [];
 const snakeSquares = [];
+const snakeSquareNumbers = [];
+let snakeHeadSquare;
+let snakeHeadNumber;
 let numberOfSnakeSquares = 3;
 let itemSquare;
 let itemSquareNumber;
+
+// Snake initialize functions
 
 const createGameBoard = () => {
 	for (let i = 0; i < numberOfSquares; i++) {
@@ -25,15 +32,17 @@ const getAllEmptySquares = () => {
 };
 
 const createSnake = () => {
-	let snakeHead = Math.floor(numberOfSquares / 2) + rowLength / 4;
+	snakeHeadNumber = Math.floor(numberOfSquares / 2) + rowLength / 4;
 	for (let i = 0; i < 3; i++) {
-		let squareNumber = snakeHead - i;
-		snakeSquares.push(squareNumber);
+		let squareNumber = snakeHeadNumber - i;
+		snakeSquareNumbers.push(squareNumber);
 		let square = allSquares[squareNumber];
-		square.classList.add('snake');
-		square.classList.remove('square');
+		convertSquareToSnake(square);
+		snakeSquares.push(square);
 		emptySquares.splice(emptySquares.indexOf(square), 1);
 	}
+	snakeHeadNumber = snakeSquareNumbers[0];
+	snakeHeadSquare = snakeSquares[0];
 };
 
 const createItem = () => {
@@ -51,6 +60,37 @@ const initializeGame = () => {
 	createSnake();
 	createItem();
 };
+
+// Common functions for snake
+
+const popSnakeTail = () => {
+	snakeSquareNumbers.pop();
+	let poppedSnakeTailSquare = snakeSquares.pop();
+	convertSnakeToSquare(poppedSnakeTailSquare);
+	emptySquares.push(poppedSnakeTailSquare);
+};
+
+const unshiftSnakeHead = (squareNumber) => {
+	snakeSquareNumbers.unshift(squareNumber);
+	let square = document.getElementById(squareNumber);
+	snakeSquares.unshift(square);
+	convertSquareToSnake(square);
+	emptySquares.splice(emptySquares.indexOf(square), 1);
+	snakeHeadNumber = snakeSquareNumbers[0];
+	snakeHeadSquare = document.getElementById(snakeHeadNumber);
+};
+
+const convertSquareToSnake = (square) => {
+	square.classList.add('snake');
+	square.classList.remove('square');
+};
+
+const convertSnakeToSquare = (square) => {
+	square.classList.add('square');
+	square.classList.remove('snake');
+};
+
+// Snake movement functions
 
 const moveSnake = (event) => {
 	let eventKey = event.key;
@@ -72,6 +112,50 @@ const moveSnake = (event) => {
 	}
 };
 
+const moveLeft = () => {
+	if (snakeSquareNumbers[1] === snakeHeadNumber - 1) {
+		return;
+	} else {
+		popSnakeTail();
+		let squareNumber = snakeHeadNumber - 1;
+		unshiftSnakeHead(squareNumber);
+	}
+};
+
+const moveRight = () => {
+	if (snakeSquareNumbers[1] === snakeHeadNumber + 1) {
+		return;
+	} else {
+		popSnakeTail();
+		let squareNumber = snakeHeadNumber + 1;
+		unshiftSnakeHead(squareNumber);
+	}
+};
+
+const moveUp = () => {
+	if (snakeSquareNumbers[1] === snakeHeadNumber - rowLength) {
+		return;
+	} else {
+		popSnakeTail();
+		let squareNumber = snakeHeadNumber - rowLength;
+		unshiftSnakeHead(squareNumber);
+	}
+};
+
+const moveDown = () => {
+	if (snakeSquareNumbers[1] === snakeHeadNumber + rowLength) {
+		return;
+	} else {
+		popSnakeTail();
+		let squareNumber = snakeHeadNumber + rowLength;
+		unshiftSnakeHead(squareNumber);
+	}
+};
+
+// Game actions
+
 initializeGame();
+
+// Event listeners
 
 document.body.addEventListener('keydown', (event) => moveSnake(event));
